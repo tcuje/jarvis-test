03e09df7b726ec1ba0f16b0792fa2a59374b83e2
Thomas Cujé <tcuje@freenet.de>
2020-01-26 20:13:37 +0100

    Initial commit

---
+++ src/test/java/de/rwth/swc/jarvis/Tests.java
@@ -0,0 +1,18 @@
+package de.rwth.swc.jarvis;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Tests {
+	
+	@Test
+	public void testStartup() {}
+	
+	@Test
+	public void testRuntime() {}
+	
+	@Test
+	public void testIntegration() {
+		Assert.assertTrue("Integration not working. You found the cause", false);
+	}
+}
